595a1318a210fa1c55ac3fe2cc1ff1abf498f7af
Rani Abu Sultan <raniabusultan@gmail.com>
2018-09-12 15:02:20 +0300

    Make use of a default value for the input property.

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ChildComponent implements OnInit {
 
   @Input('loggedIn')
-  loginFlag: boolean;
+  loginFlag = true;
 
   constructor() { }
 
